42489188d970f20c50569142e32853556dc9d2e8
Vitorios20 <vriosdantas@gmail.com>
2024-12-10 19:20:09 -0300

    branch de testes criada com snackbar MATERIAL UI

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,28 +1,58 @@
-function Item ({item, selectProduct, quantityProduct}){
+import React, { useState } from "react";
+import Snackbar from "@mui/material/Snackbar";
+import Alert from "@mui/material/Alert";
 
-    
+function Item({ item, selectProduct, quantityProduct }) {
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
 
+  const handleSelectProduct = (id) => {
+    selectProduct(id);
+    setSnackbarOpen(true);
+  };
 
-    return(
+  const handleCloseSnackbar = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setSnackbarOpen(false);
+  };
+
+  return (
     <>
-                    <div onClick={() => selectProduct(item.id)} className={`product ${item.Conditionalrendering  ? 'selected' : ''}`}>
-                    <div className="photo">
-                        <img src={"./img/" + item.photo} />
-                    </div>
-                    <div className="description">
-                        <span className="name">{item.name}</span>
-                        <span className="price">$ {item.price}</span>
+      <div
+        onClick={() => handleSelectProduct(item.id)}
+        className={`product ${item.Conditionalrendering ? "selected" : ""}`}
+      >
+        <div className="photo">
+          <img src={"./img/" + item.photo} alt={item.name} />
+        </div>
+        <div className="description">
+          <span className="name">{item.name}</span>
+          <span className="price">$ {item.price}</span>
 
-                        {item.Conditionalrendering  &&
+          {item.Conditionalrendering && (
+            <div className="quantity-area">
+              <h3>
+                <span className="descrição">Descrição do prato: </span>
+              </h3>
+              <span className="name">{item.description}</span>
+            </div>
+          )}
+        </div>
+      </div>
 
-                        <div className="quantity-area">
-                            <h3><span className="descrição">Descrição do prato: </span></h3>
-                            <span className="name">{item.description}</span>
-                        </div>
-                        }
-                    </div>
-                </div>
+      <Snackbar
+        open={snackbarOpen}
+        autoHideDuration={3000}
+        onClose={handleCloseSnackbar}
+        anchorOrigin={{ vertical: "top", horizontal: "right" }}
+      >
+        <Alert onClose={handleCloseSnackbar} severity="success" sx={{ width: "100%" }}>
+          {`Você selecionou: ${item.name}`}
+        </Alert>
+      </Snackbar>
     </>
-    )
+  );
 }
-export default Item
+
+export default Item;
